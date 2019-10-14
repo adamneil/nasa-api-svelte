035e78df238dba5467e4795db59b267df9745161
Adam Falck <adam.falck@gmail.com>
2019-10-14 04:02:20 -0500

    first commit

---
+++ src/routes/nasa.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onMount } from "svelte";
+
+  let pod;
+
+  onMount(async () => {
+    const res = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=Y2iexOoVessNH8V3BpMfzK1FErpeIGG6j5TGsOdh"
+    );
+    pod = await res.json();
+    console.log(pod);
+  });
+</script>
+
+<style>
+  .container {
+    display: grid | inline-grid;
+  }
+</style>
+
+{#if pod}
+  <div class="container">
+    <h1>{pod.explanation}</h1>
+    <img src={pod.url} alt="" />
+  </div>
+{/if}
